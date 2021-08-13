a965c7f6b2b4c049c430ddc3cd4d0e4c451c0bb0
Megha Sachin <meghahadke@yahoo.in>
2021-08-13 01:35:35 +0100

    First version of code

---
+++ src/main/java/project4/testng/gradle/Geometry.java
@@ -0,0 +1,19 @@
+package project4.testng.gradle;
+
+public class Geometry {
+	public float areaRect(float width, float height) {
+		return 0;
+	}
+	
+	public float perimeterRect(float width, float height) {
+		return 0;
+	}
+	
+	public float volumeSphere(float radius) {
+		return 0;
+	}
+	
+	public float surfaceAreaSphere(float radius) {
+		return 0;
+	}
+}
---
+++ src/test/java/project4/testng/gradle/GeometryTest.java
@@ -0,0 +1,19 @@
+package project4.testng.gradle;
+
+public class GeometryTest {
+	public void testAreaRect() {
+		
+	}
+	
+	public void testPerimeterRect() {
+		
+	}
+	
+	public void testVolumeSphere() {
+		
+	}
+	
+	public void testSurfaceAreaSphere() {
+		
+	}
+}
