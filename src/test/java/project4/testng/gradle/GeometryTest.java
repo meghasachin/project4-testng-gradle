283350553051f2cff1bc6704d4ba5810c4a67a5f
Megha Sachin <meghahadke@yahoo.in>
2021-08-15 22:06:41 +0100

    configured TestNG with Gradle with the help of https://www.petrikainulainen.net/

--- src/test/java/project4/testng/gradle/GeometryTest.java
+++ src/test/java/project4/testng/gradle/GeometryTest.java
@@ -1,18 +1,25 @@
 package project4.testng.gradle;
 
+import org.testng.annotations.Test;
+
 public class GeometryTest {
+	
+	@Test()
 	public void testAreaRect() {
 		
 	}
 	
+	@Test()
 	public void testPerimeterRect() {
 		
 	}
 	
+	@Test()
 	public void testVolumeSphere() {
 		
 	}
 	
+	@Test()
 	public void testSurfaceAreaSphere() {
 		
 	}
