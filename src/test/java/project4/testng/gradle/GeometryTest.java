1f3b6c1702f94e1b066b20879f9cf8def88e0113
Megha Sachin <meghahadke@yahoo.in>
2021-08-15 22:11:42 +0100

    Added first failing test for rect area function.

--- src/test/java/project4/testng/gradle/GeometryTest.java
+++ src/test/java/project4/testng/gradle/GeometryTest.java
@@ -1,12 +1,28 @@
 package project4.testng.gradle;
 
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class GeometryTest {
 	
+	private Geometry testObject;
+	
+	@BeforeTest
+	public void setup() {
+		testObject = new Geometry();
+	}
+	
+	/*********************************************************************************************************/
+	/*                                 			AreaRect													 */
+	/*********************************************************************************************************/
+	// test area of rectangle with even width and height
 	@Test()
 	public void testAreaRect() {
-		
+		float width = 4;
+		float height = 6;
+		float result = testObject.areaRect(width, height);
+		Assert.assertEquals(result, 24, "Test area of rectangle with even width/4 and height/6");
 	}
 	
 	@Test()
