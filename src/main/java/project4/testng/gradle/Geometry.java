c5ba486f09a308d42ea2402387ac4ad106ead775
Megha Sachin <meghahadke@yahoo.in>
2021-08-15 23:59:00 +0100

    implemented sphere volume with big decimal rounding to 2 decimals and half up.

--- src/main/java/project4/testng/gradle/Geometry.java
+++ src/main/java/project4/testng/gradle/Geometry.java
@@ -1,6 +1,11 @@
 package project4.testng.gradle;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Geometry {	
+	private static float PI = 3.14f;
+	
 	public float areaRect(float width, float height) {
 		return width * height;
 	}
@@ -10,7 +15,9 @@ public class Geometry {
 	}
 	
 	public float volumeSphere(float radius) {
-        return 0;
+		BigDecimal bigDecimal = new BigDecimal(4f/3f);
+		bigDecimal = bigDecimal.setScale(2, RoundingMode.HALF_UP);
+		return bigDecimal.floatValue() * PI * radius * radius * radius;
 	}
 	
 	public float surfaceAreaSphere(float radius) {
